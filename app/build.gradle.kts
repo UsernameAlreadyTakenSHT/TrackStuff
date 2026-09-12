c14eeb35222390f98e18e310b288301be0568de7
UsernameAlreadyTakenSHT <266476451+UsernameAlreadyTakenSHT@users.noreply.github.com>
2026-09-12 19:20:32 +0200

    Version 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
