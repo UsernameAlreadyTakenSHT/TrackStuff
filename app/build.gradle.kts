118f81d83258befdd011ef1b4ac499f95f99449d
UsernameAlreadyTakenSHT <266476451+UsernameAlreadyTakenSHT@users.noreply.github.com>
2026-09-13 20:00:29 +0200

    Version 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.example.trackstuff"
         minSdk = 29
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
