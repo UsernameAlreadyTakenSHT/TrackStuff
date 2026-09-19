697892b76e3d2e8d7c00ae20bacba34c250ddeed
UsernameAlreadyTakenSHT <266476451+UsernameAlreadyTakenSHT@users.noreply.github.com>
2026-09-19 15:51:14 +0200

    Version 0.1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "io.github.usernamealreadytakensht.trackstuff"
         minSdk = 29
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
